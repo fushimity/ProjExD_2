059b5d2e1a00d37b9fddaf10627256433bb418d4
fushimity <fushimity@edu.teu.ac.jp>
2026-06-16 14:51:56 +0900

    練習４：衝突判定

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -52,6 +52,9 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+        if kk_rct.colliderect(bb_rct):  # こうかとんRectと爆弾Rectが重なったら
+            print("ゲームオーバー")
+            return
         screen.blit(bg_img, [0, 0]) 
 
         key_lst = pg.key.get_pressed()
